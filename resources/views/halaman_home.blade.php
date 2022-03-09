c8b2ff41d6823c4b7abb17c13db73b3c6381b16d
ichsanasabila03 <ichsanasabila003@gmail.com>
2022-03-09 15:19:32 +0700

    first commit

---
+++ resources/views/halaman_home.blade.php
@@ -0,0 +1,6 @@
+<h1>
+    ICHSANA SABILA
+</h1>
+<p>
+    nama saya adalah ichsana sabila
+</p>
---
+++ resources/views/kampus/mahasiswa.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <ol>
+        <li>Ichsana Sabila</li>
+        <li>Fika Adilah</li>
+        <li>Dira Mahgfirah</li>
+        <li>Farah Salsabila</li>
+    </ol>
+<div>
+    Copyright &copy; <?php echo date("Y")?> Program Studi Teknik Informatika-PNL
+</div>
+</body>
+</html>

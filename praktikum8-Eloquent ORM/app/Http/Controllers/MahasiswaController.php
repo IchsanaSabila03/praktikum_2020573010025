168015edc6a25e4c418f487c965c46c2875a0d84
ichsanasabila03 <ichsanasabila003@gmail.com>
2022-06-08 20:50:37 +0700

    praktikum

--- praktikum8-Eloquent ORM/app/Http/Controllers/MahasiswaController.php
+++ praktikum8-Eloquent ORM/app/Http/Controllers/MahasiswaController.php
@@ -8,11 +8,11 @@ use Illuminate\Http\Request;
 class MahasiswaController extends Controller
 {
     //kode program akan kita tulis disini
-    public function cekObject(){
+    public function insert(){
         $mahasiswa = new Mahasiswa;
         $mahasiswa->nim = '19003036';
         $mahasiswa->nama = 'Ichsanasabila';
-        $mahasiswa->tanggal_lahir = '03 10 2002';
+        $mahasiswa->tanggal_lahir = '2002-10-03';
         $mahasiswa->ipk = 3.5;
         $mahasiswa->save();
         
